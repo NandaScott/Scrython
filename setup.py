0c0dd3b6e772aa0b196b12ee1776e6aaf07c440a
Nanda Scott <nanda1123@gmail.com>
2018-01-10 20:14:33 -0500

    Added setup.py and modified gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='scrython',
+	version='0.0.1',
+	description='A library to interact with the Scryfall API.',
+	url='https://github.com/NandaScott/Scrython',
+	author='Nanda Scott',
+	license='MIT',
+	packages=['cards'],
+	zip_safe=False)

117b1d4672efa9febf9c3539435be1da16bbf986
Nanda Scott <nanda1123@gmail.com>
2018-02-05 23:19:56 -0500

    Modfied gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 from setuptools import setup
 
-requirements = []
-with open('requirements.txt') as f:
-	requirements = f.read().splitlines()
-
 readme = ''
 with open('README.md') as f:
 	readme = f.read()
 
 setup(
 	name='scrython',
-	packages=['scrython'],
+	packages=['scrython', 'scrython.cards', 'scrython.rulings'],
 	version='0.1.0',
 	description='A wrapper for using the Scryfall API.',
-	long_description=readme
+	long_description=readme,
 	url='https://github.com/NandaScott/Scrython',
 	download_url='https://github.com/NandaScott/Scrython/archive/0.1.0.tar.gz',
 	author='Nanda Scott',
 	author_email='nanda1123@gmail.com',
 	license='MIT',
 	keywords=['Scryfall', 'magic', 'the gathering', 'scrython', 'wrapper'],
-	install_requires=requirements
+	install_requires=['aiohttp', 'asyncio']
 	)

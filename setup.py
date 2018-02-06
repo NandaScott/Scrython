78604b40991db8dec38c1dbdc0efcf756c17b1fe
Nanda Scott <nanda1123@gmail.com>
2018-02-05 22:32:12 -0500

    Moved files around for uploading to PyPI.

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+
+requirements = []
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+
+readme = ''
+with open('README.md') as f:
+	readme = f.read()
 
 setup(
 	name='scrython',
-	packages=['scrython']
+	packages=['scrython'],
 	version='0.1.0',
 	description='A wrapper for using the Scryfall API.',
+	long_description=readme
 	url='https://github.com/NandaScott/Scrython',
 	download_url='https://github.com/NandaScott/Scrython/archive/0.1.0.tar.gz',
 	author='Nanda Scott',
+	author_email='nanda1123@gmail.com',
 	license='MIT',
-	keywords=['Scryfall', 'magic', 'the gathering', 'scrython', 'wrapper']
+	keywords=['Scryfall', 'magic', 'the gathering', 'scrython', 'wrapper'],
+	install_requires=requirements
 	)

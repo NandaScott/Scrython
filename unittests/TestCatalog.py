c7f5a83e3a8b8ff22e3fda36156a43f7315539b0
Nanda Scott <nanda1123@gmail.com>
2018-10-23 12:28:17 -0400

    Created unit test for catalog

---
+++ unittests/TestCatalog.py
@@ -0,0 +1,27 @@
+# This workaround makes sure that we can import from the parent dir
+import sys
+sys.path.append('..')
+
+from scrython.catalog import ArtifactTypes
+import unittest
+import time
+
+artifacts = ArtifactTypes()
+
+class TestCatalog(unittest.TestCase):
+
+    def test_object(self):
+        self.assertIsInstance(artifacts.object(), str)
+        self.assertEqual(artifacts.object(), 'catalog')
+
+    def test_uri(self):
+        self.assertIsInstance(artifacts.uri(), str)
+
+    def test_total_values(self):
+        self.assertIsInstance(artifacts.total_values(), int)
+
+    def test_data(self):
+        self.assertIsInstance(artifacts.data(), list)
+
+if __name__ == '__main__':
+    unittest.main()

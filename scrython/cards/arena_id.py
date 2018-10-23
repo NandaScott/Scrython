4a7abb712b185e49ce43b95879c695f87bf658fd
Nanda Scott <nanda1123@gmail.com>
2018-10-23 17:32:44 -0400

    Updated ArenaId docstring

--- scrython/cards/arena_id.py
+++ scrython/cards/arena_id.py
@@ -5,11 +5,23 @@ class ArenaId(CardsObject):
     cards/id
     Get a card by the Arena id.
 
-    Positional arguments:
-        id : str ....................... The Scryfall Id of the card.
-
-    Optional arguments:
-        Inherits all arguments from CardsObject.
+    Args:
+        id (string):
+            The Scryfall Id of the card.
+        format (string, optional):
+            Defaults to 'json'.
+            Returns data in the specified method.
+        face (string, optional):
+            Defaults to empty string.
+            If you're using the `image` format, this will specify if you want the front or back face.
+        version (string, optional):
+            Defaults to empty string.
+            If you're using the `image` format, this will specify if you want the small, normal,
+            large, etc version of the image.
+        pretty (string, optional):
+            Defaults to empty string.
+            Returns a prettier version of the json object.
+            Note that this may break functionality with Scrython.
 
     Attributes:
         All attributes are inherited from CardsObject.
